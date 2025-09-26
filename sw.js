a19c30bc52f9510f2b2f09e31175e00a8cb04953
mahdi-Malekpour <mahdi.me.14.m@gmail.com>
2025-09-26 22:23:37 +0330

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
-const cacheName = "deepomand-cache-v1";
-const assets = [
-  "/index.html",
-  "/style.css",
-  "/app.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
-// هنگام نصب، فایل‌ها رو کش می‌کنه
-self.addEventListener("install", event => {
+self.addEventListener("install", (event) => {
+  console.log("✅ Service Worker نصب شد");
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(assets))
+    caches.open("deepomand-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./script.js",
+        "./style.css",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
+    })
   );
 });
 
-// وقتی کاربر فایل میخواد، اول از کش نگاه می‌کنه
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
